7889eb0a17e24f2b58c37ddf4d3c2687133ff14d
Brendan Nelson <brendan.nelson49@gmail.com>
2021-12-15 14:04:54 -0800

    added clear function

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -148,18 +148,8 @@ export class OrdersComponent implements OnInit {
     }
   }
 
-  // prepare result, splice last name, first name
-
-  // Calculate total and perform input validation
-
-  // display the order form with orders from orders.json
-
-  // Clear the orders form
-
-  // Add items 'Hot Dog', 'Hamberger' and 'Pizza' to list when corresponding button is clicked
-
-  // delete line item (order) when delete button is click
-
-  // read in the orders.json file and populate the list table with the initial orders (3)
+  clear() {
+    this.orders = [];
+  }
 
 }
